fa217bbf63114b826f25013ff3302dc81ed314d3
Kobi Cohen <kobic8@gmail.com>
2022-01-25 18:47:24 +0200

    before changing indexing

--- main_fin_tracker.py
+++ main_fin_tracker.py
@@ -21,6 +21,7 @@ def init_df_monthly():
         data_dict[category] = [0]*12
     df_month = pd.DataFrame(data_dict)
     df_month = df_month.transpose()
+    df_month.to_csv('Data/fin_data.csv')
     return df_month
 
 
@@ -31,7 +32,13 @@ def init_df_log():
     for col in headers:
         data_dict[col] = []
     df_log = pd.DataFrame(data_dict)
+    df_log.to_csv('Data/data_all.csv', index=False)
     return df_log
+
+
+def update_file(data_month, data_log, full_path):
+    data_month.to_csv(full_path)
+    data_log.to_csv('Data/data_all.csv')
 
 
 def init(full_path, override=False):
@@ -49,19 +56,17 @@ def init(full_path, override=False):
     else:
         df_month = init_df_monthly()
         df_log = init_df_log()
-        update_file(df_month, df_month, full_path)
+        # update_file(df_month, df_log, full_path)
     return df_month, df_log
-
-
-def update_file(data_month, data_log, full_path):
-    data_month.to_csv(full_path)
-    data_log.to_csv('Data/data_all.csv')
 
 
 def get_expense(expense_date='', expense_store='supermarket', expense_amount=0, expense_name='item',
                 expense_cat='electrics'):
     expense_name = input("what did you buy?")
-    expense_cat = input("What category?")
+    categories = ['Alt.payments', 'Const.payments', 'Transportation', 'Medical', 'Housing', 'Personal',
+                  'Entertainment', 'Gifts', 'Food', 'Income']
+    print(*enumerate(categories), sep="\n")
+    expense_cat = categories[int(input("What category? please select the number"))]
     expense_date = input("Enter purchase date [dd/mm/yyyy]\n")
     expense_store = input("where did you buy it?")
     expense_amount = int(input('How much?'))
@@ -71,33 +76,24 @@ def get_expense(expense_date='', expense_store='supermarket', expense_amount=0, 
 
 def add_item_data_log(data_log, expense):
     import pandas as pd
-    row_to_add = pd.Series(expense, index=data_log.columns)
+    indexing = data_log.columns
+    if len(expense) != len(data_log.columns):
+        indexing = data_log.columns[1:]
+    row_to_add = pd.Series(expense, index=indexing)
     data_log = data_log.append(row_to_add, ignore_index=True)
     return data_log
 
 
-def add_item_data_month(data, item):
-    # TODO change this for by month
-    day, month, year = item['Date'].split('/')
-    category = item['Category']
-    if year in data:
-        if month in data[year]:
-            if category in data[year][month]:
-                data[year][month][category]['Total'] += item['amount']
-                data[year][month][category]['Items'].append(item)
-            else:
-                data[year][month][category] = {}
-                data[year][month][category]['Total'] = item['amount']
-                data[year][month][category]['Items'] = [item]
-        else:
-            data = new_item(data, item)
-    else:
-        data[year] = {}
-        data = new_item(data, item)
-    return data
+def add_item_data_month(data_month, expense):
+    day, month, year = expense[-1].split('/')
+    category = expense[0]
+    cell = data_month.at[category, int(month)-1] # .iloc[Ncat+1, month]
+    cell += expense[1]
+    data_month.at[category, int(month) - 1] = cell
+    return data_month
 
 
-def plot_by_cat(data):
+def plot_by_cat(data_month):
     pass
 
 
@@ -105,41 +101,25 @@ def plot_total(data):
     pass
 
 
-def export_data(data_for_graph, fullpath):
-    df_to_json = {}
-    for key, value in data_for_graph.items():
-        df_to_json[key] = [str(val) for val in value]
-    result = data_for_graph.to_
-
-
-def new_item(data, item):
-    day, month, year = item['Date'].split('/')
-    category = item['Category']
-    data[year][month] = {}
-    data[year][month][category] = {}
-    data[year][month][category]['Total'] = item['amount']
-    data[year][month][category]['Items'] = [item]
-    return data
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     from os import path
 
     filename = 'fin_data.csv'
     directory = 'Data'
     fullpath = path.join(directory, filename)
-    clear = True
-    data = init(fullpath, clear)
-    data_log = init_df_log()
-    get_from_user = False
+    clear = False
+    df_month, df_log = init(fullpath, clear)
+    get_from_user = True
     while get_from_user:
         item = get_expense()
-        data = add_item(data, item)
+        df_log = add_item_data_log(df_log, item)
+        df_month = add_item_data_month(df_month, item)
         proceed = input("Got some more data? [Y/N]")
         if proceed == 'N':
             get_from_user = False
-    update_file(data, fullpath)
+            print(df_log)
+    update_file(df_month, df_log, fullpath)
+    print('Finish')
 
 
 
