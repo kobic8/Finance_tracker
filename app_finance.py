7d29e134b2d09d06eed93a5d826493cc451ef5ea
Kobi Cohen <kobic8@gmail.com>
2022-01-26 21:56:50 +0200

    updated streamlit app. Next: updates lines for data update

--- app_finance.py
+++ app_finance.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import datetime
 import main_fin_tracker as fin
-
-# Init
-# TODO: read the data from pc
+import pandas as pd
 
 # Text
 st.title("Kobic's Finance App")
+# Init
+# TODO: read the data from pc
+submit_load = st.button("Load data")
+if submit_load:
+    file_month = 'fin_data.csv'
+    file_log = 'data_all.csv'
+    directory = 'Data'
+    clear = False
+    df_month, df_log = fin.init(directory, [file_month, file_log], clear)
+    if not(df_log.empty) and not(df_month.empty):
+        st.success("Data is loaded")
 st.header("What would you like to do today?")
 
 # action
@@ -26,7 +35,48 @@ if action == "Enter new data":
         if submit_stop:
             print("update the data")
             # TODO update the data
+
 # TODO plot the grpahs
+# st.dataframe(df_month.iloc[:, df_month.columns != 'Categories'])
+df_income = df_month.iloc[-1:, df_month.columns != 'Categories']
+df_income.index = ["Income"]
+st.text(df_income.index)
+st.dataframe(df_income)
+
+df_num = df_month.iloc[:, df_month.columns != 'Categories']
+
+df_num = df_num.head(-1)
+
+df_outcome = df_num.sum().to_frame(name="Outcome").transpose()
+st.dataframe(df_outcome)
+st.text(df_outcome.index)
+
+nd_savings = df_income.values - df_outcome.values
+df_savings = pd.DataFrame(nd_savings, columns=df_outcome.columns, index=["Savings"])
+st.dataframe(df_savings)
+
+df_total = df_income.transpose()
+# df_total = df_total.append(df_outcome.transpose())
+# df_total = df_total.append(df_savings.transpose())
+df_total["Outcome"] = df_outcome.transpose().values
+df_total["Savings"] = df_savings.transpose().values
+df_total.index = ['%02d' % (month) for month in range(1, 13)]
+st.dataframe(df_total)
+
+# st.text(df_savings)
+st.bar_chart(df_total)
+st.text(df_total.columns)
+
+df_num = df_num.transpose()
+# df_num.columns = df_month.iloc[:, df_month.columns == 'Categories']
+categories = ['Alt.payments', 'Const.payments', 'Transportation', 'Medical', 'Housing', 'Personal',
+                  'Entertainment', 'Gifts', 'Food']
+df_num.columns = categories
+st.dataframe(df_num)
+
+
+st.bar_chart(df_num)
+# TODO create total outcome-income chart
             # st.write(f"hello and welcome: {date.strftime('%d/%m/%Y')}")
 # year, month, day = date.split('-')
 # st.write(f"day is {day} month is {month} and the year: {year}")
